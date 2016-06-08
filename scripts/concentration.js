a83c5a3fd94c99bf9b32be4f4fa16a6fffb8f3fc
Kevin Golden <kwg@cs.net>
2016-06-08 01:13:57 -0400

    needs final troubleshooting and formatting

--- scripts/concentration.js
+++ scripts/concentration.js
@@ -1,25 +1,12 @@
 $( document ).ready(function() {
-  imgPath="/images/";
-  stuff = document.getElementById('tile1');
-  images = ["gg1-100.jpg", "gg2-100.jpg", "gg3-100.jpg", "ev1-100.jpg", "ev2-100.jpg", "ev3-100.jpg", "mg1-100.jpg", "mg2-100.jpg"];
-  pointers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
-
-  // set up the board based on the shuffled array.  Tile 1 gets the image in
-  // element 0, tile 2 gets the image in element 1, etc.
-
-  // start button event handler
-  $("#start").on("click", function() {
-    clickOnce=0;
-    seconds=0;
-    clickedTiles = [];
-    openCells=[];
-    matches=0;
-    shuffleArray(pointers);
-    if (clickOnce < 1) {
-      timerid = setInterval(updateTime, 1000);
-      clickOnce = 1;
-    }
-  });
+  startGame();
+  imagesInput = [
+    "gg1-100.jpg", "gg2-100.jpg", "gg3-100.jpg", "ev1-100.jpg",
+    "ev2-100.jpg", "ev3-100.jpg", "mg1-100.jpg", "mg2-100.jpg"
+  ];
+  imagesArrayed = ["gg1-100.jpg", "gg2-100.jpg", "gg3-100.jpg", "ev1-100.jpg", "ev2-100.jpg", "ev3-100.jpg", "mg1-100.jpg", "mg2-100.jpg",
+  "gg1-100.jpg", "gg2-100.jpg", "gg3-100.jpg", "ev1-100.jpg", "ev2-100.jpg", "ev3-100.jpg", "mg1-100.jpg", "mg2-100.jpg"];
+  pointers = shuffleArray(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"]);
 
 });
 function shuffleArray(array) {
@@ -34,47 +21,77 @@ function shuffleArray(array) {
 function startGame() {
   //start the game-timer
   //enable clicking
+  clickOnce = 1;    // prevents this from being run on next click
+  seconds = 0;        // reset to 0 for timer
+  clickedTiles = [];   // blank array stores names of clicked images
+  openCells=[];        // blank array stores cell numbers of clicked images
+  matches=0;           // used to count matches, game wins when set to 8
+  timerid = setInterval(updateTime, 1000);  //start the game timer
+  blnkImage = "bc-100.png";
+  imgPath = "./images/";
+  openSeconds = 3000;  // milliseconds to leave pictures open before flipping them.
 
 }
-function clickTile(id) {
-  // call flipTiles for this cell
-  // update the clickedTiles and openCells arrays
-  // if it's the first click (only one element in clickedTiles), just return
-  // if it's the second click, check to see if there's a match
-  // if there's a match, update the matches variable
-  // if matches = 8, call the gameWon
-  // if if no match, leave the cells open for a certain amount of time, then
-  // call flipTiles for the open cells, reset the openCells and
-  // clickedTiles arrays and exit.
-  //
-  //
+function clickTile(cellID) {
+  // if (clickedOnce = 0) {
+  //   startGame();
+  // }
+  cellNum = cellID.substring(4);
+  flipTiles(cellID, cellNum);       // flip the clicked tile
+  pushFile = imagesArrayed[pointers[cellNum]];
+  openTiles = clickedTiles.push(pushFile);
+  openCells.push(cellNum);
+  console.log("numer of files: " + clickedTiles.length);
 
-  // var x = document.getElementsByTagName("H1")[0].getAttribute("class");
-  //  alert($(this).attr('data-val'));
-  // tileNum = this.getAttribute('src');
+  if (openTiles === 2)  {
+    if (clickedTiles[0] === clickedTiles[1]) {   // check to see if file names match
+      matches += 1;                     // update match count
+      if (match = 8) {                // if we have 8 matches we win!
+        gameWon();
+      }
+    } else {
+      setTimeout(closeAllOpen, openSeconds);   // close open cells; needs timer
+    }
 
-  var tileSrc = obj.src;
-  console.log("Tile " + tileNum + " has been clicked")
-
-}
-function changeImage() {
-  var image = document.getElementById('myImage');
-  if (image.src.match("bulbon")) {
-    image.src = "pic_bulboff.gif";
-  } else {
-    image.src = "pic_bulbon.gif";
   }
 }
+
+function flipTiles(tileID, tileNum) {
+  // tileX = cellIDx.substr(5);
+  // console.log("Cell: " + tileX);
+  tilePic = [imagesArrayed[pointers[tileNum]]];
+  var image=document.getElementById(tileID);
+  if (image.src.match(blnkImage)) {
+    image.src = imgPath + [imagesArrayed[pointers[tileNum]]];
+  } else {
+    image.src = imgPath + blnkImage;
+  }
+}
+
+function closeAllOpen() {
+  for (var i = openCells.length - 1; i > 0; i--) {
+    // tileXX = "tile" + openCells[i];
+    console.log("object: tile" + openCells[i]);
+    var imageXX=document.getElementById("tile" + openCells[i]);
+    imageXX.src = imgPath + blnkImage;
+    // flipTiles(clickedTiles[i]);
+  }
+  clickedTiles = [];   // reset names of clicked images
+  openCells=[];        //reset open cells
+
+}
+  function gameWon() {
+    // message or something
+    // display reset button
+    //stop the game timer
+    clearInterval(timerid);
+    return;
+  }
 // timer stuff
 
 var updateTime = function() {
   document.getElementById("timer").innerHTML = seconds;
   seconds+=1;
-};
-$("#reset").on("click",function(){
-  seconds = 0;
-  clearInterval(timerID);
-  $("timer").text("");
-  // document.getElementById("timer").innerHTML = "Stop Watch";
-});
+}
+
 // end timer stuff
